2d7a41f3c59c15239ac944f7e7429d5b566ac828
teto72 <tsubasa.k.729@gmail.com>
2020-05-12 12:34:54 +0900

    add(profile):自己紹介画像の追加

--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 2020_05_09_100114) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "username"
-    t.string "image"
-    t.text "text"
+    t.string "profile_image_id"
+    t.text "profile"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
