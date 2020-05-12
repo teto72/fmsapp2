5c4ffd4a785db212873350e8b15bb084fe3b221d
teto72 <tsubasa.k.729@gmail.com>
2020-05-12 16:17:01 +0900

    add(submit button):更新ボタンの追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd9fcaf1584767d0492d598d9d0d2446fc50568b9cb121fbf60b1b6bc2dc7b9b2f1e71649a1a3b6ee8fbcb4f05e659ab8fff7d85cc4f41975e10d0805dd91decf'
