c544970a3c0ff9ee7102ebb751ed2f4f739f39ae
teto72 <tsubasa.k.729@gmail.com>
2020-05-12 20:45:51 +0900

    add(create action)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,18 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to post_path(@post)
+  end
+
   def edit
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title,:text,:image)
   end
 end
