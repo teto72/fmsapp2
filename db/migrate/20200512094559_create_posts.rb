e0ed5b5ba35146e305757ce38c1420d2aba82391
teto72 <tsubasa.k.729@gmail.com>
2020-05-12 18:59:48 +0900

    add(posts_controller):postsコントローラー追加

--- db/migrate/20200512094559_create_posts.rb
+++ db/migrate/20200512094559_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.integer :user_id
+      t.string :title
+      t.string :image_id
+      t.text :text
       t.timestamps
     end
   end
