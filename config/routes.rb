81fb7b6012cff0d5c3c1ebe77df86c0cbf8d2df7
teto72 <tsubasa.k.729@gmail.com>
2020-05-12 19:11:41 +0900

    アソシエーション記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/edit'
   devise_for :users
   root to: "home#index"
   resources :users
+  resources :posts
 end
